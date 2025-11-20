027d394c81cae791f3a1e8dfa4acd8b36ea1af09
A02-blip <andysanz02@gmail.com>
2025-11-19 19:21:02 -0600

    MENÚ
    Se creó un menú para que el usuario determine el tamaño del terreno, velocidad del gusano y cuando despertar el gusano. Esto se logró con un do/while, un try/catch y un switch

--- Main.java
+++ Main.java
@@ -1,8 +1,36 @@
 public class Main {
 	public static void main(String[] args) {
-		
-		char[][] mapa;
-		mapa = new char[12][16];
+	    Scanner scanner = new Scanner(System.in);
+	    int opcion = 0, filas, columnas; 
+	    
+	    
+	   do{
+	       try{
+	           System.out.println("MENÚ");
+                System.out.println("1. TAMAÑO DEL TERRENO");
+                System.out.println("2. VELOCIDAD DEL GUSANO");
+                System.out.println("3. DESPERTAR GUSANO");
+                opcion = scanner.nextInt();
+                
+            switch(opcion){
+                case 1: 
+                    System.out.println("NÚMERO DE FILAS: "); 
+                    filas = scanner.nextInt(); 
+                    System.out.println("NÚMERO DE COLUMNAS: ");
+                    columnas = scanner.nextInt();
+                    char[][] mapa;
+		            mapa = new char[filas][columnas];
+                    break; 
+                case 2: 
+                    break; 
+                case 3: 
+                    break; 
+            }
+
+	       }catch{
+	           
+	       }
+	   }
 		
 		HiloGusano gusano = new HiloGusano(mapa);
 		MonitorMapa monitor = new MonitorMapa(mapa);
